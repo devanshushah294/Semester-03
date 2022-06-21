0a35a351c6ac0700ae58dc1e1aaa21f1f714f7c3
Devanshu Shah <93487920+devanshushah69@users.noreply.github.com>
2022-06-21 18:42:20 +0530

    Create FactorialUsingRecursion.java

---
+++ Data Structures And Algorithm/01_BasicJavaPrograms/FactorialUsingRecursion.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FactorialUsingRecursion{
+    public static int findFactorial(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        else{
+            return n*findFactorial(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int answer = findFactorial(n);
+        System.out.println(n+"!="+answer);
+    }
+}
