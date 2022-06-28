d24a686a58a68520fe2c2fe93f19416a0f45b030
Devanshu Shah <93487920+devanshushah69@users.noreply.github.com>
2022-06-28 22:59:44 +0530

    Java Programs

---
+++ Data Structures And Algorithm/JavaPrograms/Lab-03/StudentDemo.java
@@ -0,0 +1,40 @@
+// Create array of object of class Student_Detail with attributes Enrollment_no, Name, Sem, CPI for 5 students, scan their information and print it. (Home Work)
+import java.util.Scanner;
+
+class Student_Detail{
+    long enrollment_no;
+    String name;
+    byte sem;
+    double cpi;
+
+    //constructor
+    public Student_Detail(){
+        Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Enter your enrollment number");
+        enrollment_no = sc.nextLong();
+        System.out.println("Enter the your name : ");
+        name = sc2.nextLine();
+        System.out.println("Enter the semester : ");
+        sem = sc.nextByte();
+        System.out.println("Enter the cpi : ");
+        cpi = sc.nextDouble();
+        sc.close();
+    }
+
+    //methods
+    public void printStudentDetails(){
+        System.out.println("the name of student is : "+name);
+        System.out.println("the enrollment number of student is : "+enrollment_no);
+        System.out.println("The semester of the student is : "+sem);
+        System.out.println("The cpi of the given student is :"+cpi);
+    }
+}
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student_Detail s1 = new Student_Detail();
+        s1.printStudentDetails();
+    }
+    
+}
---
+++ Data Structures And Algorithm/JavaPrograms/Lab-03/SwapDemo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Swap{    
+    int n1,n2;
+    public Swap(int n1, int n2){
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+    public void swapNumbers(){
+        int extra = n1;
+        n1 = n2;
+        n2 = extra;
+    }
+    public void printNumbers(){
+        System.out.println("the number 1 is "+n1);
+        System.out.println("the number 2 is "+n2);
+    }
+}
+public class SwapDemo{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number 1 : ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter the number 2 : ");
+        int n2 = sc.nextInt();
+        Swap s1 = new Swap(n1,n2);
+        s1.swapNumbers();
+        s1.printNumbers();
+    }
+}
