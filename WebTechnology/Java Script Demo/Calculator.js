fe59126c5e06b0278ebd7f52ad8bff9faf13c0b3
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-07-09 18:05:33 +0530

    JavaScript Programs

--- WebTechnology/Java Script Demo/Calculator.js
+++ WebTechnology/Java Script Demo/Calculator.js
@@ -1,38 +1,40 @@
-function input(n){
-    a = document.getElementById("screen");
-    console.log(a);
-    a.innerHTML += n.innerHTML;
-}
-
-function opration(op){
-    a = document.getElementById("screen");
-    oldValue=parseDouble(a.innerHTML);
-    a.innerHTML="";
-    operator=op.innerHTML;
-}
-function clearScreen(this){
-    document.getElementById("screen").innerHTML = "";
-}
-
-function outPut(){
-    a = document.getElementById("screen");
-    newvalue=parseDouble(a.innerHTML);    
-    if(operator=='+'){
-        answer=newvalue+oldValue;
+    var oldValue;
+    var operator;
+    function input(n){
+        a = document.getElementById("screen");
+        if(a.innerHTML =='+'||a.innerHTML =='-'||a.innerHTML =='*'||a.innerHTML =='/'){
+            a.innerHTML = "";
+        }
+        a.innerHTML += n.innerHTML;
     }
-    else if(operator == '-'){
-        answer=oldValue-newvalue;
+    function operation(op){
+        a = document.getElementById("screen");
+        oldValue=parseFloat(a.innerHTML);
+        a.innerHTML=op.innerHTML;
+        operator=op.innerHTML;
     }
-    else if(operator == '*'){
-        answer=oldValue*newvalue;
+    function clearScreen(){
+        document.getElementById("screen").innerHTML = "";
     }
-    else if(operator == '/'){
-        answer=oldValue/newvalue;
+    function output(){
+        a = document.getElementById("screen");
+        newvalue=parseFloat(a.innerHTML);    
+        if(operator=='+'){
+            answer=newvalue+oldValue;
+        }
+        else if(operator == '-'){
+            answer=oldValue-newvalue;
+        }
+        else if(operator == '*'){
+            answer=oldValue*newvalue;
+        }
+        else if(operator == '/'){
+            answer=oldValue/newvalue;
+        }
+        a.innerHTML=answer;
     }
-    a.innerHTML=answer
-}
-function goBack(){
-    a = document.getElementById("screen");
-    str=a.innerHTML;
-    a.innerHTML=str.substring(0,str.length-1);
-}
+    function goBack(){
+        a = document.getElementById("screen");
+        str=a.innerHTML;
+        a.innerHTML=str.substring(0,str.length-1);
+    }
