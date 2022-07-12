4dcd71082ce29d63623d01da7775f04a68a8e9e6
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-07-13 00:57:27 +0530

    DBMS LAB SOLUTIONS

--- DBMS-2/Lab_02.sql
+++ DBMS-2/Lab_02.sql
@@ -1,4 +1,14 @@
 Create Database Employee_Info;
 use Employee_Info;
 
-select* from Employee
+
+--1. Display all the employees whose name starts with “m” and 4 th character is “h”.
+	Select	EID, EName 
+	From	Employee
+	Where	Ename like 'm%';
+
+--2. Find the value of 3 raised to 5. Label the column as output.
+	Select	power(3,5)
+	as 'Output';
+
+--3. Write a query to subtract 20 days from the current date.	Select	GETDATE()-20	as 'Date before 20 Days';--4. Write a query to display name of employees whose name starts with “j” and contains “n” in their name.	Select	EName 	From	Employee	where	EName like 'j%n%';--5. Display 2nd to 9th character of the given string “SQL Programming”.	Select	SUBSTRING('SQL Programming',2,9);--6. Display name of the employees whose city name ends with “ney” &contains six characters.	Select	EName	from	Employee	where	City like '%ney';--7. Write a query to convert value 15 to string.	Select	CONVERT(varchar(10),15)	As		'15 to varchar';--8. Add department column with varchar (20) to Employee table.	Alter	table	Employee	Add		department	varchar(20);--9. Set the value of department to Marketing who belongs to London city.	Update	Employee	set		Department ='Marketing' 	where	City = 'London';--10. Display all the employees whose name ends with either “n” or “y”.	Select	EName	from	Employee	where	EName like '%n' or EName like '%y';	--	or	Select	EName	from	Employee	where	EName like '%[n,y]';--11. Find smallest integer value that is greater than or equal to 63.1, 63.8 and -63.2.		
