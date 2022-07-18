fc55dcdb0b588e338258a8d116520510da17db51
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-07-18 21:01:02 +0530

    DBMS Lab Solutions

--- DBMS-2/Lab_02.sql
+++ DBMS-2/Lab_02.sql
@@ -55,4 +55,38 @@ use Employee_Info;
 --11. Find smallest integer value that is greater than or equal to 63.1, 63.8 and -63.2.
 	Select CEILING(63.1),CEILING(63.8),CEILING(-63.2);
 	
---12. Display all employees whose joining date is not available.	Select	EName 	from	Employee	where	JoiningDate is null;--13. Display name of the employees in capital letters and city in small letters.	Select UPPER(EName),LOWER(city)	from Employee;--14. Change the data type of Ename from varchar (30) to char (30).	Alter table Employee	Alter column Ename char(30);--
+--12. Display all employees whose joining date is not available.
+	Select	EName 
+	from	Employee
+	where	JoiningDate is null;
+
+--13. Display name of the employees in capital letters and city in small letters.
+	Select UPPER(EName),LOWER(city)
+	from Employee;
+
+--14. Change the data type of Ename from varchar (30) to char (30).
+	Alter table Employee
+	Alter column Ename char(30);
+
+--15. Display city wise maximum salary.
+	Select City ,Max(salary) as 'MaximumSalary'
+	from Employee
+	group by City
+
+--16. Produce output like <Ename> works at <city> and earns <salary> (In single column).
+	select RTrim(EName) + ' works at ' + City + ' and earns ' + Cast(Salary as varchar) as 'WorkingStatus'
+	from Employee
+--17. Find total number of employees whose salary is more than 5000.
+--18. Write a query to display first 4 & last 3 characters of all the employees.
+--19. List the city having total salaries more than 15000 and employees joined after 1st January, 2014.
+--20. Write a query to replace “u” with “oo” in Ename.
+--21. Display city with average salaries and total number of employees belongs to each city.
+--22. Display total salaries paid to female employees.
+--23. Display name of the employees and their experience in years.
+--24. Remove column department from employee table.
+--25. Update the value of city from syndey to null.
+--26. Retrieve all Employee name, Salary & Joining date.
+--27. Find out combine length of Ename & Gender.
+--28. Find the difference between highest & lowest salary.
+--29. Rename a column from Ename to FirstName.
+--30. Rename a table from Employee to EmpMaster.
