17ab817a3833749e324d51b6850153d8b4ecc92e
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-07-18 23:19:01 +0530

    Data Structure And Algorithms Programs

--- Data Structures And Algorithm/JavaPrograms/Lab-04/AiBiUsingStack.java
+++ Data Structures And Algorithm/JavaPrograms/Lab-04/AiBiUsingStack.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 class Stack{
-    char[] a = new char[100];
+    char[] a;
+    public Stack(int n){
+        a = new char[n];
+    }
     private int top = -1;
 
     public void push(char ch){
-        if(top>99){
-            System.out.println("Stack Underflow");
-            return;
+        if(top>a.length-1){
+            System.out.println("Stack Overflow");
         }
         else{
             top++;
             a[top] = ch;
         }
     }
-
+    
     public void pop(){
         if(top==-1){
         System.out.println("Stack Underflow");
@@ -30,11 +32,30 @@ class Stack{
 }
 public class AiBiUsingStack {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        for(int i = 0;i<str.length();i++){
-            
-        }
-    }
-    
+        Scanner sc=new Scanner(System.in);
+		System.out.println("Enter String = ");
+		String str=sc.next();
+		Stack s1=new Stack(str.length());
+		int countB=-1;
+		int length=str.length();
+		for(int i=0;i<length;i++){
+			char ch=str.charAt(i);
+			if(ch=='a'){
+				s1.push(ch);
+			}
+			else{
+				countB++;
+			}
+		}
+		// while(s1.top!=-1){
+		// 	s1.pop();
+		// 	countB--;
+		// }
+		if(countB==s1.getTop()){
+			System.out.println("Valid String");
+		}
+		else{
+			System.out.println("Not valid String");
+		}
+	}
 }
