7a6fb95df6447c19e9d02013671e623f4d011031
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-08-03 11:42:17 +0530

    Data Structure And Algorithms Programs

--- Data Structures And Algorithm/JavaPrograms/Lab-08_LinkedList/StackUsingLinkedList.java
+++ Data Structures And Algorithm/JavaPrograms/Lab-08_LinkedList/StackUsingLinkedList.java
@@ -1,39 +1,44 @@
-class Node {
-    int number;
-    Node next;
-    public Node(int n){
-        this.number = n;
-        this.next = null;
-    }
-}
-class LinkedList {
-    Node head;
-    public int getHeadNumber(){
-        if(head == null){
-            return -1;
+class LinkedList{
+    Node head,last;
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
         }
-        return head.number;
     }
-    public void insertNumberFromFirst(int number){
-        Node newNode = new Node(number); 
-        newNode.next = head;
-        head = newNode;
-    }
-
-    public void insertNumberFromLast(int number){
-        Node newNode = new Node(number);
+    public void insertFromLast(int n){
+        Node newNode = new Node(n);
         if(head == null){
             head = newNode;
+            last =newNode;
             return;
         }
-        Node currentNode = head;
-        while(currentNode.next != null){
-            currentNode = currentNode.next;
+        else{
+            last.next=newNode;
+            last=last.next;
         }
-        currentNode.next = newNode;
 
     }
-
+    public void deleteFromLast(){
+        if(head == null){
+            System.out.println("List is Empty");
+            return;
+        }
+        else{
+            if(head.next==null){
+                head=null;
+                return;
+            }
+            Node currentNode = head;
+            while(currentNode.next!= last){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = null;
+            last = currentNode;
+        }
+    }
     public void printLL(){
         if(head == null) {
             System.out.println("List is Empty");
@@ -41,74 +46,19 @@ class LinkedList {
         }
         Node currentNode = head;
         while (currentNode != null){
-            System.out.print(currentNode.number+"->");
+            System.out.print(currentNode.data+"->");
             currentNode = currentNode.next;
         }
         System.out.println("NULL");
     }
-
-    public Node deleteFromFront(int indexFromFront){
-        if(head == null){
-            return head;
-        }
-        Node currentNode = head;
-        for(int i = 1; i<indexFromFront-1; i++){
-            currentNode = currentNode.next;
-        }
-        Node returnNode = currentNode.next; 
-        if(returnNode == head){
-            head = head.next;
-        }
-        else{
-            currentNode.next=currentNode.next.next;
-        }
-        return returnNode;
-    }
-
-    public Node deleteLastElement(){
-        if(head == null){
-            return head;
-        }
-        if(head.next==null){
-            head=null;
-            return head;
-        }
-        Node currentNode = head;
-        while(currentNode.next.next!=null){
-            currentNode = currentNode.next;
-        }
-        Node returnNode = currentNode.next;
-        if(returnNode == head){
-            head = null;
-        }
-        else{
-            currentNode.next = currentNode.next.next;
-        } 
-        return returnNode;
-    }
-}
-
-class Stack {
-    LinkedList ll = new LinkedList();
-    Node top;
-    public void push(int n){
-        ll.insertNumberFromLast(n);
-    }
-    public void pop(){
-        ll.deleteLastElement();
-    }
-    public void displayStack(){
-        ll.printLL();
-    }
-
 }
 public class StackUsingLinkedList{
     public static void main(String[] args) {
-        Stack stk = new Stack();
-        stk.push(25);
-        // stk.push(36);
-        // stk.push(85);
-        stk.pop();
-        stk.displayStack();
+        LinkedList ll = new LinkedList();
+        ll.insertFromLast(25);
+        ll.insertFromLast(26);
+        ll.deleteFromLast();
+       // ll.deleteFromLast();
+        ll.printLL();
     }
 }
