3b529d007a57df130b2e4e3817f8c81e3b975c4a
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-08-23 19:47:57 +0530

    Doubly Linked List Update

---
+++ Data Structures And Algorithm/JavaPrograms/Lab-08_LinkedList/DoublyLinkedListDemo.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class DoublyLinkedList {
+    class Node {
+        Node prev;
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+    Node head;
+    public void deleteFromFirst(){
+        if(head == null){
+            System.out.println("The list is empty");
+            return;
+        }
+        else if(head.next == null){
+            head = null;
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+    }
+    public void insertFromFirst(int number) {
+        Node newNode = new Node(number);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        head.prev = newNode;
+        newNode.next = head;
+        head = newNode;
+    }
+    public void insertFromLast(int number){
+        Node newNode = new Node(number);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node currentNode = head;
+        while(currentNode.next!=null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+        newNode.prev = currentNode;
+    }
+    public void printLL(){
+        if(head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+        System.out.print("NULL<->");
+        Node currentNode = head;
+        while (currentNode != null){
+            System.out.print(currentNode.data+"<->");
+            currentNode = currentNode.next;
+        }
+        System.out.println("NULL");
+    }
+}
+
+public class DoublyLinkedListDemo{
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        DoublyLinkedList ll = new DoublyLinkedList();
+        ll.insertFromLast(25);
+        ll.insertFromLast(36);
+        ll.insertFromFirst(85);
+        ll.printLL();
+        ll.deleteFromFirst();
+        ll.deleteFromFirst();
+        ll.deleteFromFirst();
+        ll.printLL();
+    }
+}
