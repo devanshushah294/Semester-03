017fe78f6e64817d2bfa3d52e21a38d128cd992c
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-11-21 23:41:09 +0530

    MyFirstApp update

--- WebTechnology/Angular/MyFirstApp/src/app/app-routing.module.ts
+++ WebTechnology/Angular/MyFirstApp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebTechnology/Angular/MyFirstApp/src/app/app.module.ts
+++ WebTechnology/Angular/MyFirstApp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
